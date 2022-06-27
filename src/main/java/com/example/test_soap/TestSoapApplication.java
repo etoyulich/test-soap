001cbc8e4599259d7a46f9ad21727427a87d0477
Gomazkova Yulia <gomazkova19@yandex.ru>
2022-06-27 14:21:14 +0300

    init

---
+++ src/main/java/com/example/test_soap/TestSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSoapApplication.class, args);
+    }
+
+}
