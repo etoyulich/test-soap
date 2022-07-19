bc45eb4763a250376315b42ed29934c73f9e07da
Yulia Gomazkova <gomazkova19@yandex.ru>
2022-07-19 17:58:12 +0300

    снова что-то сгенерировала

--- src/main/java/com/example/springsoap/gen/newgen/ClientInterface.java
+++ src/main/java/com/example/springsoap/gen/newgen/ClientInterface.java
@@ -1,14 +1,13 @@
 
 package com.example.springsoap.gen.newgen;
 
-import jakarta.jws.WebMethod;
-import jakarta.jws.WebParam;
-import jakarta.jws.WebResult;
-import jakarta.jws.WebService;
-import jakarta.xml.ws.RequestWrapper;
-import jakarta.xml.ws.ResponseWrapper;
-
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
 import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
 
 /**
@@ -17,12 +16,11 @@ import javax.xml.bind.annotation.XmlSeeAlso;
  * Generated source version: 2.2
  * 
  */
-@javax.jws.WebService
-@WebService(name = "ClientService", targetNamespace = "http://www.example.com/springsoap/gen")
+@WebService(name = "ClientInterface", targetNamespace = "http://www.example.com/springsoap/gen")
 @XmlSeeAlso({
     ObjectFactory.class
 })
-public interface ClientService {
+public interface ClientInterface {
 
 
     /**
@@ -31,8 +29,6 @@ public interface ClientService {
      * @return
      *     returns java.lang.String
      */
-
-    @javax.jws.WebMethod
     @WebMethod
     @WebResult(name = "response", targetNamespace = "")
     @RequestWrapper(localName = "getClientRequest", targetNamespace = "http://www.example.com/springsoap/gen", className = "com.example.springsoap.gen.newgen.GetClientRequest")
