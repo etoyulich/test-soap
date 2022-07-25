9fa7ba3ad00e916b94ea8bc10994d6b3f4eb4378
Yulia Gomazkova <gomazkova19@yandex.ru>
2022-07-25 11:31:21 +0300

    added soap address

--- src/main/java/com/example/testsoap/TestSoapApplication.java
+++ src/main/java/com/example/testsoap/TestSoapApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.xml.ws.Endpoint;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class TestSoapApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(TestSoapApplication.class, args);
-        Endpoint.publish("http://localhost:8181/ws", new ClientServiceImpl());
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/main/resources/application.properties"));
+        String address = properties.getProperty("soap.address");
+        Endpoint.publish(address, new ClientServiceImpl());
     }
 
 }
